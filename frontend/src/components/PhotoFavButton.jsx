5f62caefd817b05048ddc0802d51431a416436f8
Cynthia <cynthia-lam@outlook.com>
2024-05-21 17:27:15 -0400

    Implement functionality to allow users to favourite photos

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,18 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [isLiked, setLiked] = useState(false);
+
+  const toggleLiked = function() {
+    console.log("Fav button clicked!");
+    console.log(isLiked);
+    setLiked(!isLiked);
+  }
+
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+      <div className="photo-list__fav-icon-svg" onClick={toggleLiked}>
+        <FavIcon selected={isLiked}/>
       </div>
     </div>
   );
