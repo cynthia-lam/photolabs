f7a7daddf7de099ceef021265d12041801846415
Cynthia <cynthia-lam@outlook.com>
2024-05-23 17:44:09 -0400

    Refactor to import photos and topics mock data at App.jsx and pass it down to each component instead of importing it right at the child component

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -4,11 +4,11 @@ import PhotoList from './PhotoList';
 import '../styles/HomeRoute.scss';
 
 // Note: Rendering a single component to build components in isolation
-const HomeRoute = () => {
+const HomeRoute = (props) => {
   return (
     <div className="home-route">
-      <TopNavigationBar />
-      <PhotoList />
+      <TopNavigationBar topics={props.topics}/>
+      <PhotoList photos={props.photos}/>
     </div>
   );
 };
