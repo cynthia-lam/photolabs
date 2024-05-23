268b28bad726da338033bf44983d46cddd9bec23
Cynthia <cynthia-lam@outlook.com>
2024-05-22 23:30:48 -0400

    Replace default props with mock data photos.js

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
+import photos from "mocks/photos";
 
 const sampleDataForPhotoList = [
   {
@@ -60,7 +61,7 @@ const sampleDataForPhotoList = [
 const PhotoList = () => {
   return (
     <ul className="photo-list">
-      {sampleDataForPhotoList.map(photo =>
+      {photos.map(photo =>
         <PhotoListItem key={photo.id} photo={photo}/>
       )}
     </ul>
