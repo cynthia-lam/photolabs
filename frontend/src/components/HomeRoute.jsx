96c77ed6acdea178b73d26face7e7820a98cda8e
Cynthia <cynthia-lam@outlook.com>
2024-05-24 14:58:05 -0400

    Implement global state favCount using useReducer

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import { useReducer, React } from 'react';
 import TopNavigationBar from './TopNavigationBar';
 import PhotoList from './PhotoList';
 import '../styles/HomeRoute.scss';
 
 // Note: Rendering a single component to build components in isolation
 const HomeRoute = (props) => {
+  const [favCount, dispatch] = useReducer((favCount, amount) => {
+    return favCount + amount;
+    }, 0)
+
   return (
     <div className="home-route">
-      <TopNavigationBar topics={props.topics}/>
-      <PhotoList photos={props.photos}/>
+      <TopNavigationBar topics={props.topics} isFavPhotoExist={favCount > 0 ? true : false}/>
+      <PhotoList photos={props.photos} updateFavCount={dispatch}/>
     </div>
   );
 };
