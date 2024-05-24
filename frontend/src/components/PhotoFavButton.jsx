b63f4f8c2edd08d5e71e152f8551b5c16f490232
Cynthia <cynthia-lam@outlook.com>
2024-05-24 14:59:48 -0400

    Update toggleLiked to also updateFavCount

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,13 +3,19 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton(props) {
   const [isLiked, setLiked] = useState(false);
 
   const toggleLiked = function() {
     console.log("Fav button clicked!");
     console.log(isLiked);
     setLiked(!isLiked);
+    
+    if (!isLiked) {              // if it wasn't already liked, toggling it should add one to favCount
+      props.updateFavCount(1);
+    } else {                     // if it wasn liked, toggling it should subtract one to favCount
+      props.updateFavCount(-1);
+    }
   }
 
   return (
