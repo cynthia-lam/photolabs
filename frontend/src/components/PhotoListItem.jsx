ea71ced3de41bde94b965dc70a4772b46a93b841
Cynthia <cynthia-lam@outlook.com>
2024-05-25 14:59:31 -0400

    Change to pass down toggleFavourites

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -9,7 +9,7 @@ const PhotoListItem = (props) => {
 
   return (
     <div className="photo-list__item">
-      <PhotoFavButton updateFavCount={props.updateFavCount}/>
+      <PhotoFavButton id={id} toggleFavourite={props.toggleFavourite}/>
       <img src={urls.full} className="photo-list__image" />
       <div className="photo-list__user-details">
         <img src={user.profile} className="photo-list__user-profile" />
