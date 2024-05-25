ca1a34da7b5d502466bdb50c5b789e2c0fa52821
Cynthia <cynthia-lam@outlook.com>
2024-05-25 17:02:58 -0400

    Add PhotoDetailsModal component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import HomeRoute from './components/HomeRoute';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    isModalOpen ? 
+    setModalOpen(false) :
+    setModalOpen(true)
+  };
+
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics}/>
+      <HomeRoute photos={photos} topics={topics} toggleModal={toggleModal}/>
+      <PhotoDetailsModal isModalOpen={isModalOpen}/>
     </div>
   );
 };
