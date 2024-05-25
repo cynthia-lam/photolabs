3006e1a076b1f9373b0547bb8439e2f7f8421032
Cynthia <cynthia-lam@outlook.com>
2024-05-25 14:59:05 -0400

    Change to pass down toggleFavourites and favourites

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -1,20 +1,35 @@
-import { useReducer, React } from 'react';
+import { useReducer, React, useState } from 'react';
 import TopNavigationBar from './TopNavigationBar';
 import PhotoList from './PhotoList';
 import '../styles/HomeRoute.scss';
 
-// Note: Rendering a single component to build components in isolation
 const HomeRoute = (props) => {
-  const [favCount, dispatch] = useReducer((favCount, amount) => {
-    return favCount + amount;
-    }, 0)
+  const [favourites, setFavourites] = useState([]);
+
+  const isFavourite = function(id) {
+    return favourites.includes(id);
+  }
+
+  // adds or removes id from favourites array
+  const toggleFavourite = function(id) {
+    if (isFavourite(id)) {
+      favourites.pop(id);
+    } else {
+      favourites.push(id);
+    }
+  }
 
   return (
     <div className="home-route">
-      <TopNavigationBar topics={props.topics} isFavPhotoExist={favCount > 0 ? true : false}/>
-      <PhotoList photos={props.photos} updateFavCount={dispatch}/>
+      <TopNavigationBar topics={props.topics} isFavPhotoExist={favourites.length > 0 ? true : false}/>
+      <PhotoList photos={props.photos} toggleFavourite={toggleFavourite} favourites={favourites}/> 
     </div>
   );
 };
 
 export default HomeRoute;
+
+//should never pass dispatch
+//ALWAYS pass interface fxns - need to pass all 3 (2 fxns + list) to photolist, components only needs to know how to toggle itself
+// google interface WHAT v implementation HOW (secret!!!)
+// think about it like inc, dec, clear
