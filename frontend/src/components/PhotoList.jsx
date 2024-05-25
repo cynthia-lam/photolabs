7e43a6f5f908af79b0b2fef9b6c6f13a68e4ab17
Cynthia <cynthia-lam@outlook.com>
2024-05-25 14:59:20 -0400

    Change to pass down toggleFavourites

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -4,10 +4,11 @@ import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
 
 const PhotoList = (props) => {
+  console.log("favs: ", props.favourites);
   return (
     <ul className="photo-list">
       {props.photos.map(photo =>
-        <PhotoListItem key={photo.id} photo={photo} updateFavCount={props.updateFavCount}/>
+        <PhotoListItem key={photo.id} photo={photo} toggleFavourite={props.toggleFavourite}/>
       )}
     </ul>
   );
