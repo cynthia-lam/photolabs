1531349072d9d47b41e08a3140569889eae99240
Cynthia <cynthia-lam@outlook.com>
2024-05-26 12:53:43 -0400

    Remove comments

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,7 +4,6 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton(props) {
-  // const [isLiked, setLiked] = useState(false);
   const {id, favourites, isFavourite} = props;
 
   const favourited = isFavourite(id);
