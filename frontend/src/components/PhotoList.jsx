d74e3b9ad0b00fc213a3b515414e090814168cce
Cynthia <cynthia-lam@outlook.com>
2024-05-25 22:45:26 -0400

    Remove console log

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -4,7 +4,6 @@ import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
 
 const PhotoList = (props) => {
-  console.log("favs: ", props.favourites);
   return (
     <ul className="photo-list">
       {props.photos.map(photo =>
