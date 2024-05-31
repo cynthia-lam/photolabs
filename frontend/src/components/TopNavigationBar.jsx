449fe950c2dff3171a6061bbc15e7fa2ef1bb8ac
Cynthia <cynthia-lam@outlook.com>
2024-05-31 00:09:07 -0400

    Change styling of Nav to include topics and fav in one div, make it align right

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -8,8 +8,10 @@ const TopNavigation = (props) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <div className="top-nav-bar__topics_and_fav">
       <TopicList topics={props.topics} setTopicId={props.setTopicId}/>
       <FavBadge isFavPhotoExist={props.isFavPhotoExist} selected={props.isFavPhotoExist}/>
+      </div>
     </div>
   )
 }
